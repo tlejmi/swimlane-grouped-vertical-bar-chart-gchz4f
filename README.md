0cb2e066f1b5363064e5d5c408cef73723017771
tlejmi <tarek.lejmi@gmail.com>
2021-11-01 23:49:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swimlane-grouped-vertical-bar-chart-gchz4f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swimlane-grouped-vertical-bar-chart-gchz4f)
---
+++ app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { multi } from './data';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  multi: any[];
+  view: any[] = [700, 400];
+
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Country';
+  showYAxisLabel: boolean = true;
+  yAxisLabel: string = 'Population';
+  legendTitle: string = 'Years';
+
+  colorScheme = {
+    domain: ['#5AA454', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { multi })
+  }
+
+ onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
